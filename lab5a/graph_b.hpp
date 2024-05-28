d8d9681cef05abfe9e14a0b7b8c2887638c43a06
Danil <shulgin-03@list.ru>
2024-05-28 20:58:55 +0300

    lab5a

---
+++ lab5a/graph_b.hpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph_Alg{
+    private:
+    bool *visited;
+	vector<vector<int>> graph;
+	int size_gr;
+    int size_edge;
+
+    public:
+    Graph_Alg(int size){
+        size_gr = size;
+        graph.reserve(size);
+    }
+
+    void add(int src, int dst){
+        graph[src].push_back(dst);
+    }
+    void add2(int src, int dst){
+        graph[src].push_back(dst);
+        graph[dst].push_back(src);
+        size_edge++;
+    }
+
+    void Taryan(int start){
+        visited = new bool[size_gr];
+        vector<int> stack;
+        for (int i = 0; i < size_gr; i++) visited[i] = false;
+
+        for (int v=0; v < size_gr; v++) 
+            if(visited[v] == false) dfs_inv(v, visited, stack);
+        for (int i = stack.size()-1; i > -1; i--) cout << stack[i] << " ";
+        cout << endl;
+    }
+
+    void dfs_inv(int vert, bool *visited, vector<int> &stack){
+        visited[vert] = true;
+        for (auto &y: graph[vert]){
+            if(visited[y] == false && y!=-1) dfs_inv(y, visited, stack);
+            }
+        stack.push_back(vert);
+    }
+
+    void Fleri(){
+        vector<vector<int>> tmp(size_gr);
+        for (int i = 0; i < size_gr; i++){
+            for (int j = 0; j < graph[i].size(); j++){
+                tmp[i].push_back(graph[i][j]);
+            }
+        }
+        int v = 0;
+        vector<int> result;
+        result.push_back(v);
+
+        while (size_edge > 0){
+            int res = 0;
+            for (int i = 0; i < tmp[v].size(); i++){
+                int u = tmp[v][i];
+                if (tmp[u].size() > 1){
+                    result.push_back(u);
+
+                    tmp[v].erase(tmp[v].begin() + i);
+                    auto it = find(tmp[u].begin(), tmp[u].end(), v);
+                    tmp[u].erase(it);
+                    v = u;
+                    size_edge--;
+                    res = 1;
+                    break;
+                }
+            }
+            if (res == 0){
+                for (int i = 0; i < size_gr; i++){
+                    if (tmp[v].size() < 2){
+                        for (int i = 0; i < tmp[v].size(); i++){
+                            int u = tmp[v][i];
+                            result.push_back(u);
+                            tmp[v].erase(tmp[v].begin() + i);
+                            auto it = find(tmp[u].begin(), tmp[u].end(), v);
+                            tmp[u].erase(it);
+                            size_edge--;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (auto &a: result){
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+
+    void Cycle_Alg(){
+        vector<vector<int>> tmp(size_gr);
+        for (int i = 0; i < size_gr; i++){
+            for (int j = 0; j < graph[i].size(); j++){
+                tmp[i].push_back(graph[i][j]);
+            }
+        }
+        int v = 0;
+        vector<int> result;
+        FindEulerPath(v, tmp, result);
+        for (auto &a: result){
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+
+    void FindEulerPath(int v, vector<vector<int>> &tmp, vector<int> &result){
+        for (int i = 0; i < tmp[v].size(); i++){
+            int u = tmp[v][i];
+            tmp[v].erase(tmp[v].begin() + i);
+            auto it = find(tmp[u].begin(), tmp[u].end(), v);
+            tmp[u].erase(it);
+            FindEulerPath(u, tmp, result);
+        }
+        result.push_back(v);
+    }
+
+    void Kosaraju(){
+        visited = new bool[size_gr];
+        vector<vector<int>> inv_graph(size_gr);
+        vector<int> result;
+        vector<int> stack;
+        for (int i = 0; i < size_gr; i++) visited[i] = false;
+        for (int i = 0; i < size_gr; i++){
+            for(int j = 0; j < graph[i].size(); j++){
+                int n = graph[i][j];
+                inv_graph[n].push_back(i);
+            }
+        }
+        for (int v = 0; v < size_gr; v++){
+            if (visited[v] == false) dfs_inv(v, visited, stack);
+        }
+
+        cout << "G(inv): " << endl;
+        for (auto &a: stack){
+            cout << a << " ";
+        }
+        cout << endl;
+
+        for (int i = 0; i < size_gr; i++) visited[i] = false;
+        while(stack.size() > 0){
+            int v = stack.back();
+            stack.pop_back();
+            if (visited[v] == false) dfs_inv(v, visited, result);
+        }
+
+        cout << "G: " << endl;
+        for (auto &a: result){
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+};
---
+++ lab5a/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "graph_b.hpp"
+
+int main(){
+    Graph_Alg gr(7);
+    gr.add(0, 1);
+    gr.add(0, 2);
+    gr.add(1, 3);
+    gr.add(2, 3);
+    gr.add(2, 5);
+    gr.add(3, 6);
+    gr.add(4, 5);
+    gr.add(5, 6);
+    //gr.add(6, -1);
+    cout << "Taryan: " << endl;
+    gr.Taryan(0);
+    cout << endl;
+
+    
+
+    //Test for Cycle_alg
+    Graph_Alg gr1(6);
+    gr1.add2(0, 1);
+    gr1.add2(0, 4);
+    gr1.add2(1, 2);
+    gr1.add2(1, 5);
+    gr1.add2(1, 3);
+    gr1.add2(2, 3);
+    gr1.add2(2, 4);
+    gr1.add2(2, 5);
+    gr1.add2(3, 4);
+    gr1.add2(3, 5);
+    gr1.add2(4, 5);
+    cout << "Cycle_Alg: " << endl;
+    gr1.Cycle_Alg();
+    cout << endl;
+
+    cout << "Fleri: " << endl;
+    gr1.Fleri();
+    cout << endl;
+
+    Graph_Alg gr2(8);
+    gr2.add(0, 1);
+    gr2.add(0, 3);
+    gr2.add(1, 2);
+    gr2.add(1, 4);
+    gr2.add(1, 6);
+    gr2.add(2, 5);
+    gr2.add(2, 6);
+    gr2.add(3, 0);
+    gr2.add(4, 3);
+    gr2.add(4, 5);
+    gr2.add(4, 7);
+    gr2.add(5, 6);
+    gr2.add(6, 2);
+    gr2.add(6, 7);
+    cout << "Kosaraju: " << endl;
+    gr2.Kosaraju();
+
+    return 0;
+}
